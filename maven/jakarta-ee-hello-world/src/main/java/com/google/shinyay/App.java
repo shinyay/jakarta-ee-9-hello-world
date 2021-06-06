458543df20288fbc0b19f604a5c3005a10abed62
shinyay <shinya.com@gmail.com>
2021-06-06 13:35:42 +0900

    Add servlet class

--- maven/jakarta-ee-hello-world/src/main/java/com/google/shinyay/App.java
+++ maven/jakarta-ee-hello-world/src/main/java/com/google/shinyay/App.java
@@ -1,10 +1,18 @@
 package com.google.shinyay;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
 public class App extends HttpServlet
 {
-    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.getOutputStream().println("Hello, Jakarta");
+    }
 }
